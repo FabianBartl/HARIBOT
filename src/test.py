510fc93d826ade6e52dbbbfd0e77c4c164872783
Fabian <fabian@informatic-freak.de>
2022-10-06 17:30:07 +0200

    depends on svgexport

--- src/test.py
+++
@@ -1,8 +0,0 @@
-
-import selenium
-
-svgFile = r"C:\Users\fabia\OneDrive\Dokumente\GitHub\HARIBOT\tmp\score.svg"
-pngFile = r"C:\Users\fabia\OneDrive\Dokumente\GitHub\HARIBOT\tmp\score.png"
-outDir  = r"C:\Users\fabia\OneDrive\Dokumente\GitHub\HARIBOT\tmp"
-
-
