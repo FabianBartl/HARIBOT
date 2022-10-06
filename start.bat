2868665efb85f6191788b24e947aedcb10311ef1
Fabian <fabian@informatic-freak.de>
2022-10-06 17:23:24 +0200

    add node.js nodevars.bat to launcher for #25

--- start.bat
+++ start.bat
@@ -1,6 +1,32 @@
-@echo off
-cd src
-cls
+
+@REM copied from "C:\Program Files\nodejs\nodevars.bat"
+
+@REM Ensure this Node.js and npm are first in the PATH
+set "PATH=%APPDATA%\npm;%~dp0;%PATH%"
+
+setlocal enabledelayedexpansion
+pushd "%~dp0"
+
+@REM Figure out the Node.js version.
+set print_version=.\node.exe -p -e "process.versions.node + ' (' + process.arch + ')'"
+for /F "usebackq delims=" %%v in (`%print_version%`) do set version=%%v
+
+rem Print message.
+if exist npm.cmd (
+  echo Your environment has been set up for using Node.js !version! and npm.
+) else (
+  echo Your environment has been set up for using Node.js !version!.
+)
+
+popd
+endlocal
+
+@REM If we're in the Node.js directory, change to the user's home dir.
+if "%CD%\"=="%~dp0" cd /d "%HOMEDRIVE%%HOMEPATH%"
+
+@REM -------------------------------------------------------------------------
+
+cd C:\Users\fabia\OneDrive\Dokumente\GitHub\HARIBOT\src
 
 set /a i=0
 set /a max=10
