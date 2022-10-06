69967a22913ef1519f638489a192ec8f84a3b769
Fabian <fabian@informatic-freak.de>
2022-10-06 20:57:02 +0200

    /score default png format, not ephemeral, delete tmp files after sending #15

--- src/main.py
+++ src/main.py
@@ -292,7 +292,7 @@ async def sc_ping(interaction: Interaction):
 async def sc_memberInfo(
 	interaction: Interaction
 	, member: Member = SlashOption(required=False)
-	, formatID: int = SlashOption(required=False, choices={"SVG": 0, "PNG": 1}, default=0, name="format")
+	, formatID: int = SlashOption(required=False, choices={"SVG": 0, "PNG": 1}, default=1, name="format")
 ):
 	LOG.LOGGER.debug(f"(command sent) score: {member=}")
 	
@@ -304,10 +304,10 @@ async def sc_memberInfo(
 
 	score_card_file = createScoreCard(member)
 	
-	await interaction.response.send_message(file=File(score_card_file(format)), ephemeral=True)
+	await interaction.response.send_message(file=File(score_card_file(format)), ephemeral=CONFIG.EPHEMERAL)
 
-	# os.system(f"del {score_card_file('*')}")
-	# LOG.LOGGER.warning(f"deleted temp score card files `{score_card_file('*')}`")
+	os.system(f"del {score_card_file('*')}")
+	LOG.LOGGER.warning(f"deleted temp score card files `{score_card_file('*')}`")
 
 #-----#
 
